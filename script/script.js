b328e2f19aa7b9810bdee487e3c944d3d6bb9816
LEISLY PINO <lepidu@live.com>
2022-05-17 13:14:44 +0100

    change variables

--- script/script.js
+++ script/script.js
@@ -12,24 +12,20 @@ window.onload = function() {
         end_loader()
     }, 500)
 
-    const eletricity_bill_Form = document.getElementById('calculate-eletricity_bill-form')
+    const eletricity_bill_Form = document.getElementById('bill_form')
     eletricity_bill_Form.addEventListener('submit', function(e) {
         e.preventDefault()
         start_loader();
-        const numberUnits = document.getElementById('number-of-Units').value;
-        const BillingPeriod = document.getElementById('billing-period').value;        
-        const PayableYears = document.getElementById('vat').value;
-        var monthly = 0,
-            totalPayable = 0,
-            total = 0,
-            totalInterest = 0,
-          amountOfBillWithoutVAT = 0;
-        amountOfBillWithoutVAT = ((parseFloat(numberUnits) * 0.20) + (parseFloat(BillingPeriod)  * 0.04));
-        totalPayable = (parseFloat(amountOfBillWithoutVAT) + (parseFloat(amountOfBillWithoutVAT)* 13.5)/100);
+        const numberUnits = document.getElementById('billing_units').value;
+        const BillingPeriod = document.getElementById('billing_period').value;
+        var totalPayable = 0,
+            amountOfBillWithoutVAT = 0;
+            amountOfBillWithoutVAT = ((parseFloat(numberUnits) * 0.20) + (parseFloat(BillingPeriod)  * 0.04));
+            totalPayable = (parseFloat(amountOfBillWithoutVAT) + (parseFloat(amountOfBillWithoutVAT)* 13.5)/100);
         
         setTimeout(() => {
-            document.getElementById('number-of-units').textContent = parseFloat(numberUnits).toLocaleString("en-US")
-            document.getElementById('annual-interest').textContent = parseFloat(interest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
+            document.getElementById('amount_bill').textContent = parseFloat(amountOfBillWithoutVAT).toLocaleString("en-US")
+            document.getElementById('total_payable').textContent = parseFloat(totalPayable).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
             
             document.getElementById('result').style.display = 'table';
             document.getElementById('reset-btn').style.display = 'block';
@@ -40,6 +36,11 @@ window.onload = function() {
     eletricity_bill_Form.addEventListener('reset', function(e) {
         start_loader();
         setTimeout(() => {
+
+            document.getElementById('amount_bill').textContent = ""
+            document.getElementById('total_payable').textContent = ""
+            document.getElementById('result').style.display = 'none';
+            document.getElementById('reset-btn').style.display = 'none';
            
             end_loader()
         }, 500)
