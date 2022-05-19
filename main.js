e8044026a5099f5ca860521ead9d3b0a26a14d5b
LEISLY PINO <lepidu@live.com>
2022-05-19 12:44:14 +0100

    update main.js

--- main.js
+++ main.js
@@ -1,53 +1,62 @@
 'use strict';
-const electron = require("electron");
+const {app, BrowserWindow}= require("electron");
 
-const window = electron;
-
-window.start_loader = function() {
-    const loader = document.getElementById('loader-holder')
-    loader.style.display = 'flex';
+function createWindow(){
+    const win = new BrowserWindow({
+        width: 500,
+        height:760
+    })
+    win.loadFile('index.html')
 }
-window.end_loader = function() {
-    const loader = document.getElementById('loader-holder')
-    loader.style.display = 'none';
-}
-
-window.onload = function() {
-    setTimeout(() => {
-        end_loader()
-    }, 500)
+app.whenReady().then(() => {
+    
+    createWindow()
+    payable()
+})
+    
+function payable(){
 
     const eletricity_bill_Form = document.getElementById('bill_form')
-    eletricity_bill_Form.addEventListener('submit', function(e) {
-        e.preventDefault()
-        start_loader();
+    eletricity_bill_Form.addEventListener('submit', totalAmount)
+}
+    
+    function getValues(){
         const numberUnits = document.getElementById('billing_units').value;
-        const BillingPeriod = document.getElementById('billing_period').value;
+        const billingPeriod = document.getElementById('billing_period').value;
+        return{numberUnits, billingPeriod}
+    }
+    
+    function totalAmount(e){
+        e.preventDefault();
+        const {numberUnits,billingPeriod}= getValues();
         var totalPayable = 0,
             amountOfBillWithoutVAT = 0;
             amountOfBillWithoutVAT = ((parseFloat(numberUnits) * 0.20) + (parseFloat(BillingPeriod)  * 0.04));
             totalPayable = (parseFloat(amountOfBillWithoutVAT) + (parseFloat(amountOfBillWithoutVAT)* 13.5)/100);
-        
-        setTimeout(() => {
-            document.getElementById('amount_bill').textContent = parseFloat(amountOfBillWithoutVAT).toLocaleString("en-US")
-            document.getElementById('total_payable').textContent = parseFloat(totalPayable).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
-            
-            document.getElementById('result').style.display = 'table';
-            document.getElementById('reset-btn').style.display = 'block';
-            end_loader()
-        }, 500)
+            UI(amountOfBillWithoutVAT, totalPayable)
+    }
 
-    })
-    eletricity_bill_Form.addEventListener('reset', function(e) {
-        start_loader();
-        setTimeout(() => {
+    function UI(amountOfBillWithoutVAT, totalPayable){
+        const result = document.getElementById('result').style.display = 'table';
+        var amountVAT= document.getElementById('amount_bill').textContent = parseFloat(amountOfBillWithoutVAT).toLocaleString("en-US")
+        var total = document.getElementById('total_payable').textContent = parseFloat(totalPayable).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
+        const dataPrint = document.createElement('div')
 
-            document.getElementById('amount_bill').textContent = ""
-            document.getElementById('total_payable').textContent = ""
-            document.getElementById('result').style.display = 'none';
-            document.getElementById('reset-btn').style.display = 'none';
-           
-            end_loader()
-        }, 500)
-    })
-}
+        dataPrint.innerHTML= `
+        <div class="container-data row">
+            <div class="col s4">
+                <h6>${amountVAT}</h6>
+            </div>
+            <div class="col s4">
+                <h6>${total}</h6>
+            </div>
+        </div>    
+        `
+        result.appendChild(dataPrint)
+
+        reset();
+    }
+
+    function reset(){
+        document.getElementById('form_bill').reset();
+    }
