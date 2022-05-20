10b99e2c2d98e584d79a53afe9c92829e1ce88dc
LEISLY PINO <lepidu@live.com>
2022-05-20 01:07:58 +0100

    updates files

--- main.js
+++ main.js
@@ -1,62 +1,27 @@
 'use strict';
-const {app, BrowserWindow}= require("electron");
+const {app, BrowserWindow, ipcMain}= require('electron');
+
+let win;
 
 function createWindow(){
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 500,
         height:760
     })
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+    ipcMain.on('submit', (event, amountOutVAT, amountVAT) => {
+        win.webContents.send('total_payable', amountOutVAT, amountVAT);
+    })
 }
 app.whenReady().then(() => {
     
-    createWindow()
-    payable()
+    createWindow();
 })
     
-function payable(){
+app.on('window-all-closed',()=>{
+    if (process.platform !== 'darwin')app.quit();
+})
 
-    const eletricity_bill_Form = document.getElementById('bill_form')
-    eletricity_bill_Form.addEventListener('submit', totalAmount)
-}
-    
-    function getValues(){
-        const numberUnits = document.getElementById('billing_units').value;
-        const billingPeriod = document.getElementById('billing_period').value;
-        return{numberUnits, billingPeriod}
-    }
-    
-    function totalAmount(e){
-        e.preventDefault();
-        const {numberUnits,billingPeriod}= getValues();
-        var totalPayable = 0,
-            amountOfBillWithoutVAT = 0;
-            amountOfBillWithoutVAT = ((parseFloat(numberUnits) * 0.20) + (parseFloat(BillingPeriod)  * 0.04));
-            totalPayable = (parseFloat(amountOfBillWithoutVAT) + (parseFloat(amountOfBillWithoutVAT)* 13.5)/100);
-            UI(amountOfBillWithoutVAT, totalPayable)
-    }
 
-    function UI(amountOfBillWithoutVAT, totalPayable){
-        const result = document.getElementById('result').style.display = 'table';
-        var amountVAT= document.getElementById('amount_bill').textContent = parseFloat(amountOfBillWithoutVAT).toLocaleString("en-US")
-        var total = document.getElementById('total_payable').textContent = parseFloat(totalPayable).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
-        const dataPrint = document.createElement('div')
 
-        dataPrint.innerHTML= `
-        <div class="container-data row">
-            <div class="col s4">
-                <h6>${amountVAT}</h6>
-            </div>
-            <div class="col s4">
-                <h6>${total}</h6>
-            </div>
-        </div>    
-        `
-        result.appendChild(dataPrint)
-
-        reset();
-    }
-
-    function reset(){
-        document.getElementById('form_bill').reset();
-    }
+            
